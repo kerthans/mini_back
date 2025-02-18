f48614089f42c6c7609dd9217fe05a7146332973
cyw79 <2862662294@qq.com>
2025-02-18 17:12:48 +0800

    define models for messages table

--- models/message.py
+++ models/message.py
@@ -7,11 +7,22 @@ class Message(Base):
     __tablename__ = 'messages'
 
     id = Column(Integer, primary_key=True)
-    sender_id = Column(Integer, ForeignKey('users.id'))
-    receiver_id = Column(Integer, ForeignKey('users.id'))
-    content = Column(String)
-    timestamp = Column(DateTime)
-    status = Column(String, default='unread')
+    sender_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    receiver_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    content = Column(String(1000), nullable=False)
+    timestamp = Column(DateTime, default=datetime.utcnow, nullable=False)
+    status = Column(String(20), default='unread')
 
     sender = relationship('User', foreign_keys=[sender_id])
     receiver = relationship('User', foreign_keys=[receiver_id])
+
+
+
+
+
+
+
+
+
+
+
