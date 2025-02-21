ccb3bd2f3103ef48c2c3ede560851a4dc3e9838f
xiaomayi-bee <524684501@qq.com>
2025-02-22 01:15:55 +0800

    Add files via upload

--- api_routes/v1/stuff_borrow.py
+++ api_routes/v1/stuff_borrow.py
@@ -1,15 +1,53 @@
-from flask import jsonify, request
+from flask import jsonify,request # type: ignore
 from . import api_v1
 
-@api_v1.route('/stuff-borrow', methods=['GET'])
-def get_borrow_records():
-    return jsonify({"message": "Get all borrow records"})
+#统计借物总数
+@api_v1.route('/stuff-borrow/statistic',methods=['GET'])
+def get_borrow_statistic():
+    return jsonify({"message":"Get all borrow records"})
 
-@api_v1.route('/stuff-borrow', methods=['POST'])
-def create_borrow_record():
-    data = request.get_json()
-    return jsonify({"message": "Create borrow record", "data": data}), 201
+#删除某条特定的借物申请
+@api_v1.route('/stuff-borrow/<str:sb_id>',methods=['DELETE'])
+def delete_stuff_borrow(sb_id):
+    return jsonify({"message":f"Cancel booking {sb_id}"}),204
 
-@api_v1.route('/stuff-borrow/<int:record_id>/return', methods=['PUT'])
-def return_stuff(record_id):
-    return jsonify({"message": f"Return stuff for record {record_id}"})
+#获取用户的借物历史
+@api_v1.route('/stuff-borrow/user/<str:userid>/history',methods=['GET'])
+def get_borrow_history(userid):
+    page=request.args.get('page',1,type=int)
+    size=request.args.get('size',10,type=int)
+    return jsonify({"message":f"Get {userid} borrow history"})
+
+#更新某一条借物申请
+@api_v1.route('/stuff-borrow/<str:sb_id>',methods=['PUT'])
+def update_borrow_request(sb_id):
+    data=request.get_json()
+    return jsonify({"message":f"Update {sb_id} borrow request","data":data}),200
+
+#获取某条借物申请条目
+@api_v1.route('/stuff-borrow/<str:sb_id>',methods=['GET'])
+def get_borrow_request(sb_id):
+    return jsonify({"message":f"Get {sb_id} borrow request"})
+
+#获取借物总表
+@api_v1.route('/stuff-borrow',methods=['GET'])
+def get_borrow_record():
+    page=request.args.get('page',1,type=int)
+    size=request.args.get('size',10,type=int)
+    state=request.args.get('state',None,type=int)
+    return jsonify({"message":"Get borrow record"})
+
+#获取某个用户的借物总表
+@api_v1.route('/stuff-borrow/user/<str:userid>',methods=['GET'])
+def get_user_borrow_record():
+    page=request.args.get('page',1,type=int)
+    size=request.args.get('size',10,type=int)
+    state=request.args.get('state',None,type=int)
+    return jsonify({"message":"Get user borrow record"})
+
+#增加一条借物申请条目
+@api_v1.route('/stuff-borrow',methods=['POST'])
+def create_borrow_request():
+    data=request.get_json()
+    return jsonify({"message":"Successful application","data":data}),200
+    
